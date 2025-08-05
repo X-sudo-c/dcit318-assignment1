5af200ffb259c6116cc37fa6d1f0d5325cf3a927
X-sudo-c <x1dragonn@gmail.com>
2025-08-05 11:21:30 +0000

    Ticket Price Calculator

---
+++ Ticket price calculator/Ticket price calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TicketPriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Ticket_Price = 10;
+            Console.WriteLine("Welcome to the Ticket Price Calculator!");
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age <= 12 || age >= 65)
+            {
+                Ticket_Price -= 7;
+            }
+
+            Console.WriteLine($"Your ticket price is: {Ticket_Price}");
+        }
+    }
+}
