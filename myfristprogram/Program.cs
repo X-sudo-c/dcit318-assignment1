7cf698e4ec56cf71d4b4b6d66cdd9ba591bb7735
X-sudo-c <x1dragonn@gmail.com>
2025-08-27 23:06:43 +0000

    active

---
+++ myfristprogram/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //array = a variable that can staor multiple values . fixed size
+
+            String[] cars = { "BMW", "Mustang", "Corvette" };
+
+            cars[0] = "Tesla";
+
+
+            for (int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine(cars[i]);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
